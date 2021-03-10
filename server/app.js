3b89bbacc93ed70006fcb4c584c1ad3e47f103af
Hackdroid <raza.11409652@lpu.in>
2021-03-10 23:28:08 +0530

    Added

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+app.use(cors())
 // app.use(cors({ origin: true }));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
