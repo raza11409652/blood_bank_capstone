348f3a15144e2aa85ce9329fd4d15ea4b5c5522f
Hackdroid <raza.11409652@lpu.in>
2021-03-11 11:25:53 +0530

    origin issue

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const dotEnv = require('dotenv')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
-app.use(cors())
+// app.use(cors())
 // app.use(cors({ origin: true  }));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
