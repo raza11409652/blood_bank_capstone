6df4801a9691e3881b8af552be44b2e97f9eb0ae
Hackdroid <raza.11409652@lpu.in>
2021-03-11 10:46:31 +0530

    bug fixed

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 app.use(cors())
-// app.use(cors({ origin: true }));
+// app.use(cors({ origin: true  }));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 dotEnv.config()
